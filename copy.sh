74028672396b5aec8de82c52dbe29d38b5a1eb10
yuduyeol <dy.yu@gopizza.kr>
2023-05-12 23:27:01 +0900

    change

--- copy.sh
+++ copy.sh
@@ -5,13 +5,18 @@
 # docker cp settings/face.sh aistt:/aistt/AISmartToppingTable/Face/run.sh
 # docker cp settings/vat.sh aistt:/aistt/AISmartToppingTable/Vat/run.sh
 
-for e in "ls -ltrh /dev/video*"
-do
-    echo $e
-done
+# for e in "ls -ltrh /dev/video*"
+# do
+#     echo $e
+# done
 
-if [ -e "/dev/video*" ] ; then
-    echo 'test1'
-else
-    echo 'test2'
-fi
+# if [ -e "/dev/video*" ] ; then
+#     echo 'test1'
+# else
+#     echo 'test2'
+# fi
+
+for I in /sys/class/video4linux/*;
+do
+    cat $I/name;
+done
