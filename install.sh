ae0c4b59c502a63c90323af3e1073c6584ca5466
yuduyeol <dy.yu@gopizza.kr>
2023-05-15 23:20:31 +0900

    bug fix

--- install.sh
+++ install.sh
@@ -11,3 +11,6 @@ sudo systemctl restart docker
 sudo usermod -a -G docker $USER
 
 xhost local:root
+
+echo 'SUBSYSTEM=="usb", ATTRS{idVendor}=="03e7", MODE="0666"' | sudo tee /etc/udev/rules.d/80-movidius.rules
+sudo udevadm control --reload-rules && sudo udevadm trigger
