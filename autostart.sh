dd8b92facd8d942909a221bb45d254edf33c12a0
yuduyeol <dy.yu@gopizza.kr>
2023-07-24 19:05:07 +0900

    bug fix

--- autostart.sh
+++ autostart.sh
@@ -1,4 +1,4 @@
-docker restart aistt
+docker restart manage
 docker restart node_redis
-docker exec aistt sh /aistt/run.sh
+docker exec manage sh /aistt/run.sh
 # google-chrome-stable http://192.168.0.62:3000/customer-screen -kiosk --password-store=basic
