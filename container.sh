72f93632156248adef25330f543c07bccf4eb80a
yuduyeol <dy.yu@gopizza.kr>
2023-07-24 18:47:07 +0900

    bug fix

--- container.sh
+++ container.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 docker login
-docker image pull futureplanning/aistt:1.0
+docker image pull futureplanning/aistt:manage
 docker image pull redis
+docker image pull nginx
 
-docker tag futureplanning/aistt:1.0 aistt:1.0
+#docker tag futureplanning/aistt:1.0 aistt:1.0
 
 echo 'SUBSYSTEM=="usb", ATTRS{idVendor}=="03e7", MODE="0666"' | sudo tee /etc/udev/rules.d/80-movidius.rules
 sudo udevadm control --reload-rules && sudo udevadm trigger
