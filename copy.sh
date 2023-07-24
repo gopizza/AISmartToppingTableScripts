96c0321a486be1672acd2fc1650106a9d0bd101e
yuduyeol <dy.yu@gopizza.kr>
2023-07-24 19:21:38 +0900

    bug fix

--- copy.sh
+++ copy.sh
@@ -1,2 +1,2 @@
-docker cp settings/camera.env aistt:/aistt/AISmartToppingTable/Camera/.env
-docker cp settings/face.env aistt:/aistt/AISmartToppingTable/Face/.env
+docker cp settings/camera.env manage:/aistt/AISmartToppingTable/Camera/.env
+docker cp settings/face.env manage:/aistt/AISmartToppingTable/Face/.env
