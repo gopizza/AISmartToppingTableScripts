bf007b6d5458ba0636443b4e81753f76f72d2903
yuduyeol <dy.yu@gopizza.kr>
2023-07-26 21:22:56 +0900

    bug fix

--- autostart.sh
+++ autostart.sh
@@ -6,6 +6,5 @@ docker restart manage
 docker exec manage sh /aistt/run.sh
 
 docker restart guide
-docker run --name guide --gpus all -p 3000:3000 -p 5000:5000 -d aistt:guide
 
 # google-chrome-stable http://192.168.0.62:3000/customer-screen -kiosk --password-store=basic
