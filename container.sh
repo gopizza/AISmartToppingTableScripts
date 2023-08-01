128f21342bf51df9ffcb1e2401bc9997fa2ec0c9
yuduyeol <dy.yu@gopizza.kr>
2023-08-01 13:49:27 +0900

    bug fix

--- container.sh
+++ container.sh
@@ -61,4 +61,4 @@ docker run --name node_redis -d -p 6379:6379 redis
 docker run --name node_mongodb -d -p 27017:27017 mongo
 docker run --name node_nginx -d nginx
 docker run --name guide --gpus all -e GUIDE_FRONT_BRANCH=$GUIDE_FRONT_BRANCH -p 3000:3000 -p 5000:5000 -d aistt:guide
-gnome-terminal -- bash -c "sh \"$HOME/autostart.sh\"; exec bash -i"
+echo "gnome-terminal -- bash -c \"sh \\\"$HOME/project/autostart.sh\\\"; exec bash -i\"" >> $HOME/.profile
