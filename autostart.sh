4417a91685b56f3be784128e33a2bb9def614dcf
yuduyeol <dy.yu@gopizza.kr>
2023-08-01 13:45:40 +0900

    bug fix

--- autostart.sh
+++ autostart.sh
@@ -1,5 +1,5 @@
 docker restart node_redis
-docker restart node_mongodb
+#docker restart node_mongodb
 docker restart node_nginx
 
 docker restart manage
@@ -8,4 +8,3 @@ docker exec manage sh /aistt/run.sh
 docker restart guide
 
 # google-chrome-stable http://192.168.0.62:3000/customer-screen -kiosk --password-store=basic
-# google-chrome-stable http://192.168.0.62:3000/status -kiosk --password-store=basic
--- container.sh
+++ container.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 HOST='localhost'
-GUIDE_FRONT_BRANCH='store/singapore'
+#GUIDE_FRONT_BRANCH='store/singapore'
+GUIDE_FRONT_BRANCH='store/caseA'
 
 docker login
 docker image pull futureplanning/aistt:manage
 docker image pull futureplanning/aistt:guide
 docker image pull redis
 docker image pull nginx
-docker image pull mongo
 
 docker tag futureplanning/aistt:manage aistt:manage
 docker tag futureplanning/aistt:guide aistt:guide
@@ -18,7 +18,7 @@ sudo udevadm control --reload-rules && sudo udevadm trigger
 # 권한부여
 # --privileged 
 
-CMD="docker run -it --name manage --gpus all -e HOST=$HOST --net host --ipc host -d"
+CMD="docker run -it --privileged --name manage --gpus all -e HOST=$HOST --net host --ipc host -d"
 
 # set here the path to the directory containing your videos
 VIDEOPATH="/dev/video*" 
@@ -32,34 +32,25 @@ do
     fi
 done
 
-CMD+=" --device-cgroup-rule=\'c 189:* rmw\' \
+CMD+=" --device-cgroup-rule='c 189:* rmw' \
 -e DISPLAY=unix$DISPLAY \
 -v /dev/snd:/dev/snd \
 -v /etc/localtime:/etc/localtime:ro \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
 -v /dev/bus/usb:/dev/bus/usb \
--v /var/run/wpa_supplicant:/var/run/wpa_supplicant
+-v /var/run/dbus:/var/run/dbus \
+-v /var/run/NetworkManager:/var/run/NetworkManager \
 aistt:manage /bin/bash"
 
-# echo "xhost local:root"
-# echo $CMD
-# echo "docker run --name node_redis -d -p 6379:6379 redis"
-# # echo "docker run --name node_mongodb -v ~/data:/data/db -d -p 27017:27017 mongo"
-# echo "docker run --name node_mongodb -d -p 27017:27017 mongo"
-# echo "docker run --name node_nginx -d nginx"
-# echo "docker run --name guide --gpus all -e GUIDE_FRONT_BRANCH=$GUIDE_FRONT_BRANCH -p 3000:3000 -p 5000:5000 -d aistt:guide"
+echo "xhost local:root"
+echo $CMD
+echo "docker run --name node_redis -d -p 6379:6379 redis"
+# echo "docker run --name node_mongodb -v ~/data:/data/db -d -p 27017:27017 mongo"
+echo "docker run --name node_mongodb -d -p 27017:27017 mongo"
+echo "docker run --name node_nginx -d nginx"
+echo "docker run --name guide --gpus all -e GUIDE_FRONT_BRANCH=$GUIDE_FRONT_BRANCH -p 3000:3000 -p 5000:5000 -d aistt:guide"
 
-# echo "docker exec aistt sh /aistt/camera.sh"
-# echo "docker exec aistt sh /aistt/run.sh"
+echo "docker exec aistt sh /aistt/camera.sh"
+echo "docker exec aistt sh /aistt/run.sh"
 
-# echo "gnome-terminal -- bash -c \"sh \\\"$HOME/project/autostart.sh\\\"; exec bash -i\""
-
-xhost local:root
-$CMD
-docker run --name node_redis -d -p 6379:6379 redis
-docker run --name node_mongodb -d -p 27017:27017 mongo
-docker run --name node_nginx -d nginx
-
-docker run --name guide --gpus all -e GUIDE_FRONT_BRANCH=$GUIDE_FRONT_BRANCH -p 3000:3000 -p 5000:5000 -d aistt:guide
-
-echo "gnome-terminal -- bash -c \"sh \\\"$HOME/project/autostart.sh\\\"; exec bash -i\"" >> $HOME/.profile
+echo "gnome-terminal -- bash -c \"sh \\\"$HOME/project/autostart.sh\\\"; exec bash -i\""
