13762f8f4e2b302a17e53be0e6741bd8e44a49e1
yuduyeol <dy.yu@gopizza.kr>
2023-09-04 10:33:02 +0900

    bug fix

--- autostart.sh
+++ autostart.sh
@@ -1,6 +1,6 @@
 PASSWORD=''
 
-echo $PASSWORD | sudo -S apt-get -y update && sudo -S apt-get -y update
+echo $PASSWORD | sudo -S DEBIAN_FRONTEND=noninteractive apt-get -y update && sudo -S DEBIAN_FRONTEND=noninteractive apt-get -y update
 
 docker restart node_redis
 docker restart node_mongodb
