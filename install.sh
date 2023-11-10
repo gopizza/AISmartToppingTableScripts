d7305357cf07115bbe6be42030ad48f0cb18b4ad
yuduyeol <dy.yu@gopizza.kr>
2023-11-11 04:33:38 +0900

    add install

--- install.sh
+++ install.sh
@@ -14,5 +14,9 @@ sudo usermod -a -G docker $USER
 
 sudo apt -y update
 sudo apt -y upgrade
+
+sudo apt -y install ffmpeg net-tools vim openssh-server
+sudo ufw allow 22
+
 cp autostart.sh ..
 sudo reboot
