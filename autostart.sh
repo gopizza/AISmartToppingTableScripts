0f96fa24ab9010fd59d37f89a3f24069595fa736
yuduyeol <dy.yu@gopizza.kr>
2024-01-09 11:45:39 +0900

    bug fix

--- autostart.sh
+++ autostart.sh
@@ -7,10 +7,11 @@ xhost local:root
 # docker restart node_mongodb
 # docker restart node_nginx
 
-docker restart manage
+docker restart aistt
 # docker exec manage sh /aistt/run.sh
 
 # docker restart guide
 
+docker logs --tail 20 -f aistt
 # google-chrome-stable http://192.168.0.62:3000/customer-screen --new-window -incognito -start-fullscreen --password-store=basic
 # google-chrome-stable http://192.168.0.62:3000/status --new-window -incognito -start-fullscreen --password-store=basic
