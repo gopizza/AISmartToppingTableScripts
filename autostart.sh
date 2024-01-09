051e4edecf9fbb7b389ceaf444b3c16080608ee0
yuduyeol <dy.yu@gopizza.kr>
2024-01-09 12:03:05 +0900

    bug fix

--- autostart.sh
+++ autostart.sh
@@ -12,6 +12,6 @@ docker restart aistt
 
 # docker restart guide
 
-docker logs --tail 20 -f aistt
-# google-chrome-stable http://192.168.0.62:3000/customer-screen --new-window -incognito -start-fullscreen --password-store=basic
-# google-chrome-stable http://192.168.0.62:3000/status --new-window -incognito -start-fullscreen --password-store=basic
+docker logs --tail 20 -f aistt &
+# google-chrome-stable http://192.168.0.62:3000/customer-screen --new-window -incognito -start-fullscreen --password-store=basic &
+# google-chrome-stable http://192.168.0.62:3000/status --new-window -incognito -start-fullscreen --password-store=basic &
