7911efc005027c5e66bc3759b9e7254e94256abc
yuduyeol <dy.yu@gopizza.kr>
2024-01-09 11:03:31 +0900

    bug fix

--- autostart.sh
+++ autostart.sh
@@ -3,14 +3,14 @@ PASSWORD=''
 echo $PASSWORD | sudo -S DEBIAN_FRONTEND=noninteractive apt-get -y update && sudo -S DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
 
 xhost local:root
-docker restart node_redis
-docker restart node_mongodb
-docker restart node_nginx
+# docker restart node_redis
+# docker restart node_mongodb
+# docker restart node_nginx
 
 docker restart manage
-docker exec manage sh /aistt/run.sh
+# docker exec manage sh /aistt/run.sh
 
-docker restart guide
+# docker restart guide
 
 # google-chrome-stable http://192.168.0.62:3000/customer-screen --new-window -incognito -start-fullscreen --password-store=basic
 # google-chrome-stable http://192.168.0.62:3000/status --new-window -incognito -start-fullscreen --password-store=basic
