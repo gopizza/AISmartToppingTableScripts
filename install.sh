9d09f0c176c3caf387728083cb28f8a9283f55fc
yuduyeol <dy.yu@gopizza.kr>
2024-01-30 22:09:23 +0900

    bug fix

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 sudo apt-get update
 sudo apt-get install -y ca-certificates curl gnupg lsb-release
 
-# sudo apt -y install nvidia-driver-510
-sudo apt -y install nvidia-driver-470
+sudo apt -y install nvidia-driver-510
 
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
