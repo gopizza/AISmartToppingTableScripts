216d9c34ceb5f5d4de5f5d8a20c59add24732f13
yuduyeol <dy.yu@gopizza.kr>
2024-04-12 03:13:24 +0900

    bug fix

---
+++ run.sh
@@ -0,0 +1,22 @@
+echo "AISTT Start!!"
+
+PROJECT_NAME="AISmartToppingTableScripts"
+GIT_URI="git@github.com:gopizza/$PROJECT_NAME.git"
+PROJECT_DIRECTORY="/home/gopizza/project"
+echo "PROJECT NAME : $PROJECT_NAME"
+echo "GIT URL : $GIT_URI"
+echo "PROJECT DIR : $PROJECT_DIRECTORY"
+echo 
+
+# Download project according repository GIT
+echo "Download Project from GIT"
+if [ ! -d "$PROJECT_DIRECTORY/$PROJECT_NAME" ] ; then
+    cd "$PROJECT_DIRECTORY" && git clone "$GIT_URI" "$PROJECT_DIRECTORY/$PROJECT_NAME" --verbose
+    cd "$PROJECT_DIRECTORY/$PROJECT_NAME"
+else
+    cd "$PROJECT_DIRECTORY/$PROJECT_NAME"
+    git stash && git pull && git stash pop
+fi
+echo
+
+bin/python3 $PROJECT_DIRECTORY/check.py
