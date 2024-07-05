23b94de9e393f998794d6ccbe658bd49e44d5441
yuduyeol <dy.yu@gopizza.kr>
2024-07-06 04:46:08 +0900

    bug fix

--- container.sh
+++ container.sh
@@ -9,7 +9,6 @@ if [ ! -d "/home/gopizza/Record" ]; then
     mkdir "/home/gopizza/Record"
 fi
 
-docker login
 docker pull $IMAGE
 
 echo 'SUBSYSTEM=="usb", ATTRS{idVendor}=="03e7", MODE="0666"' | sudo tee /etc/udev/rules.d/80-movidius.rules
