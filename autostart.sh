8c7f9daf6ce94f3bf7bd3e31f6cdf72990dbd5b1
gopizza <dy.yu@gopizza.kr>
2025-05-13 15:47:22 +0900

    bug fix

--- autostart.sh
+++ autostart.sh
@@ -2,14 +2,9 @@
 # echo $PASSWORD | sudo -S DEBIAN_FRONTEND=noninteractive apt-get -y update && sudo -S DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
 
 xhost local:root
-# docker restart node_redis
-# docker restart node_mongodb
-# docker restart node_nginx
 
 docker restart aistt
 docker exec aistt sh /aistt/AISmartToppingTable/run.sh
-
-# docker restart guide
 
 # docker logs --tail 20 -f aistt &
 # google-chrome-stable http://192.168.0.62:3000/customer-screen --new-window -incognito -start-fullscreen --password-store=basic &
