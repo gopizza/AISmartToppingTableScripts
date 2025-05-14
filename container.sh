fd974f58e2eae81a9135600768a8c51c2a81eecf
gopizza <dy.yu@gopizza.kr>
2025-05-14 15:25:45 +0900

    bug fix

--- container.sh
+++ container.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 STORE_INDEX='1'
 NAME='aistt'
+IMAGE='futureplanning/aistt:aistt'
+COUNTRY_CODE='KR'
 
-bash manage.sh $STORE_INDEX $NAME
+bash manage.sh $STORE_INDEX $NAME $IMAGE $COUNTRY_CODE
